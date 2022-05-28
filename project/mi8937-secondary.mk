ccfbd1e8fba19548d655a2a950de654a01894d3b
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-28 20:09:46 +0800

    project: Add mi8937-secondary

---
+++ project/mi8937-secondary.mk
@@ -0,0 +1,8 @@
+# top level project rules for the mi8937 project
+#
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+include $(LOCAL_DIR)/msm8937-secondary.mk
+
+BOARD_NAME := mi8937-secondary
+DEFINES += PROJECT_MI8937_SECONDARY=1
