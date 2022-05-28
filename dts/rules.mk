7deeffa3008fb90e92415b4ff877130c1647ee37
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-28 20:08:43 +0800

    dts: Add mi8937 devices

--- dts/rules.mk
+++ dts/rules.mk
@@ -23,11 +23,6 @@ DTBS += \
 	$(LOCAL_DIR)/sdm632-motorola-ocean.dtb \
 	$(LOCAL_DIR)/sdm632-xiaomi-onclite.dtb
 endif
-ifeq ($(PROJECT), msm8952-secondary)
-DTBS += \
-	$(LOCAL_DIR)/msm8940-xiaomi-santoni.dtb \
-	$(LOCAL_DIR)/msm8940-xiaomi-ugg.dtb
-endif
 ifeq ($(PROJECT), msm8917-secondary)
 DTBS += \
 	$(LOCAL_DIR)/msm8917-lenovo-tbx304.dtb \
@@ -36,4 +31,15 @@ endif
 ifeq ($(PROJECT), msm8937-secondary)
 DTBS += \
 	$(LOCAL_DIR)/msm8937-nokia-nd1.dtb
+endif
+
+ifeq ($(PROJECT), mi8937-secondary)
+DTBS += \
+	$(LOCAL_DIR)/msm8917-xiaomi-common.dtb \
+	$(LOCAL_DIR)/msm8917-xiaomi-riva.dtb \
+	$(LOCAL_DIR)/msm8917-xiaomi-ugglite.dtb \
+	$(LOCAL_DIR)/msm8937-xiaomi-land.dtb \
+	$(LOCAL_DIR)/msm8937-xiaomi-prada.dtb \
+	$(LOCAL_DIR)/msm8940-xiaomi-santoni.dtb \
+	$(LOCAL_DIR)/msm8940-xiaomi-ugg.dtb
 endif
