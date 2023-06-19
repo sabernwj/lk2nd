a07bb63a39e1b687fa2e006d09d7c869c8820ef5
me-cafebabe <me.cafebabe@gmail.com>
2023-06-20 00:28:26 +0800

    [TEMP] Test moto8937 stuff on mi8937

--- dts/rules.mk
+++ dts/rules.mk
@@ -26,7 +26,6 @@ DTBS += \
 	$(LOCAL_DIR)/msm8937-nokia-nd1.dtb
 endif
 
-ifeq ($(PROJECT), mi8937-secondary)
 DTBS += \
 	$(LOCAL_DIR)/msm8917-xiaomi-common.dtb \
 	$(LOCAL_DIR)/msm8917-xiaomi-riva.dtb \
@@ -34,7 +33,6 @@ DTBS += \
 	$(LOCAL_DIR)/msm8937-xiaomi-prada.dtb \
 	$(LOCAL_DIR)/msm8940-xiaomi-santoni.dtb \
 	$(LOCAL_DIR)/msm8940-xiaomi-ugg.dtb
-endif
 ifeq ($(PROJECT), mi439-secondary)
 DTBS += \
 	$(LOCAL_DIR)/sdm439-xiaomi-pine.dtb
