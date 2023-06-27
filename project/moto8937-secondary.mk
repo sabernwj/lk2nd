a361041a281291474028172d82cfefc7e2733bc6
me-cafebabe <me.cafebabe@gmail.com>
2023-06-28 02:55:02 +0800

    project: moto8937: Add 1k of padding to the DTBs
    * As per stock kernel.

--- project/moto8937-secondary.mk
+++ project/moto8937-secondary.mk
@@ -6,3 +6,7 @@ include $(LOCAL_DIR)/msm8937-secondary.mk
 
 BOARD_NAME := moto8937-secondary
 DEFINES += PROJECT_MOTO8937_SECONDARY=1
+
+# Add 1k of padding to the DTBs to allow for environment variables
+# to be runtime added by the bootloader (i.e. /chosen properties)
+DTC_FLAGS := -p 1024
