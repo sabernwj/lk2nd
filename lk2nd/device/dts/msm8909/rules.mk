d75f98badc404736bc96a5f4f545fa28f55795fb
me-cafebabe <me.cafebabe@gmail.com>
2023-08-23 06:53:17 +0800

    dts: Add Alcatel OneTouch 5051D POP 4 Slate

--- lk2nd/device/dts/msm8909/rules.mk
+++ lk2nd/device/dts/msm8909/rules.mk
@@ -1,5 +1,8 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
+ADTBS += \
+	$(LOCAL_DIR)/msm8909-alcatel-5051d.dtb
+
 QCDTBS += \
 	$(LOCAL_DIR)/msm8909-coolpad-5360-1.dtb \
 	$(LOCAL_DIR)/msm8909-coolpad-5360-2.dtb
